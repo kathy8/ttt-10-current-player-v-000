06083dfc3f89a8f33caf9b88770c2c86ec58d19a
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-08 17:45:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,7 +7,7 @@ end
 end
 count
 end
-
+#Current player method returns "X" if it's that it that players turn or "O" if it's the other players turn.
 def current_player(board)
 turn_count(board).even? ? "X":"O"
 end
